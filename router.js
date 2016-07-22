0be5777a7cc7ea59330d3f2c2853286be0ec6e56
lizzielashark <lizzie.s.sullivan@gmail.com>
2016-07-22 15:19:10 +1200

    diving deeper into data

--- router.js
+++ router.js
@@ -1,20 +1,22 @@
-// var request = require('superagent')
-// var dotenv = require('dotenv')
+var request = require('superagent');
+var env = require('dotenv').config();
 
-// console.log("welcome to the console!")
-//
-//
-// dotenv.load()
-//
-// var client = new Nationbuilder ({
-//   access_token=process.env.access_token
-// });
-//
-//
-// client.get("https://kristingillies.nationbuilder.com/api/v1/people/2", function(err, data){
-//   if (err) console.log(err)
-//
-//   console.log("line 11!")
-//
-//   console.log(data)
-// } )
+console.log("welcome to the console!")
+
+// var accessToken = process.env.access_token
+
+request
+  .get('https://kristingillies.nationbuilder.com/api/v1/people/match?email=kristin@forpurpose.co.nz&access_token=209761d26ceb76a83161a58045cc90ad8c2fdc608e7714381a9c9e055fec249e')
+ //  .send({ name: 'Manny', species: 'cat' })
+  .set('process.env.access_token', 'kristingillies')
+  .set('Accept', 'application/json')
+  .end(function(err, res){
+    if (err) {
+      console.log('Oh no! error', err );
+    } else {
+      console.log('yay got ' + JSON.stringify(res.body))
+      console.log('Diving Deeper: ' + JSON.stringify(res.body.person.first_name),
+                                      JSON.stringify(res.body.person.last_name),
+                                      JSON.stringify(res.body.person.phone));
+    }
+  });
--- server.js
+++ server.js
@@ -1,53 +1,5 @@
 var express = require('express')
 var app = express()
-var request = require('superagent');
-var env = require('dotenv').config();
-var nationbuilder = require('nationbuilder');
-var OAuth2 = nationbuilder.auth.OAuth2;
-
-console.log("welcome to the console!")
-
-var accessToken = process.env.access_token
-
-nationbuilder.options({
-    params: {
-        slug: 'kristingillies',
-    },
-    auth: nationbuilder
-});
-
-// request.get("https://kristingillies.nationbuilder.com/api/v1/people?access_token=209761d26ceb76a83161a58045cc90ad8c2fdc608e7714381a9c9e055fec249e", function(err, data){
-//   if (err) console.log(err)
-//
-//   console.log(data)
-// })
-// request
-//    .get('https://kristingillies.nationbuilder.com/api/v1/people?access_token=209761d26ceb76a83161a58045cc90ad8c2fdc608e7714381a9c9e055fec249e')
-//   //  .send({ name: 'Manny', species: 'cat' })
-//    .set('process.env.access_token', 'kristingillies')
-//    .set('Accept', 'application/json')
-//    .end(function(err, res){
-//      if (err) {
-//        console.log('Oh no! error', err );
-//      } else {
-//        console.log('yay got ' + JSON.stringify(res.body));
-//      }
-//    });
-
-   request
-      .get('https://kristingillies.nationbuilder.com/api/v1/people/match?email=kristin@forpurpose.co.nz&access_token=209761d26ceb76a83161a58045cc90ad8c2fdc608e7714381a9c9e055fec249e')
-     //  .send({ name: 'Manny', species: 'cat' })
-      .set('process.env.access_token', 'kristingillies')
-      .set('Accept', 'application/json')
-      .end(function(err, res){
-        if (err) {
-          console.log('Oh no! error', err );
-        } else {
-          console.log('yay got ' + JSON.stringify(res.body.person.phone));
-        }
-      });
-
-// \'Accept\' and \'Content-Type\' to \'application/json\'
 
 app.get('/', function (req, res) {
   res.send('Nationbuilder bitchez')
