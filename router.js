be7a1c66516ac97e13498e74f33e5bfeb8f40d5e
lizzielashark <lizzie.s.sullivan@gmail.com>
2016-07-22 16:22:43 +1200

    posting to endpoint working

--- router.js
+++ router.js
@@ -1,22 +1,47 @@
 var request = require('superagent');
 var env = require('dotenv').config();
 
-console.log("welcome to the console!")
+console.log("You Are Cool.")
 
-// var accessToken = process.env.access_token
 
 request
   .get('https://kristingillies.nationbuilder.com/api/v1/people/match?email=kristin@forpurpose.co.nz&access_token=209761d26ceb76a83161a58045cc90ad8c2fdc608e7714381a9c9e055fec249e')
- //  .send({ name: 'Manny', species: 'cat' })
-  .set('process.env.access_token', 'kristingillies')
   .set('Accept', 'application/json')
   .end(function(err, res){
     if (err) {
       console.log('Oh no! error', err );
     } else {
-      console.log('yay got ' + JSON.stringify(res.body))
+      //console.log('yay got ' + JSON.stringify(res.body))
       console.log('Diving Deeper: ' + JSON.stringify(res.body.person.first_name),
                                       JSON.stringify(res.body.person.last_name),
                                       JSON.stringify(res.body.person.phone));
     }
   });
+
+
+//////*****Create New Person****//////
+var postNow = function() {
+
+request
+  .post('https://kristingillies.nationbuilder.com/api/v1/people?access_token=209761d26ceb76a83161a58045cc90ad8c2fdc608e7714381a9c9e055fec249e')
+  .set('Accept', 'application/json')
+  .send({
+  "person": {
+    "email": "test@test.com",
+    "last_name": "Test",
+    "first_name": "Lizzie",
+    "sex": "M",
+    "employer": "Testingtons",
+  }
+})
+  .end(function(err, res){
+    if (err) {
+      console.log("I'm sorry, there's been an error: ", err)
+    } else {
+      console.log("success: ", res)
+    }
+  })
+
+}
+
+//postNow();
