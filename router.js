01f16eba417aa828c20edc06da36fa5954345b55
lizzielashark <lizzie.s.sullivan@gmail.com>
2016-07-26 16:11:25 +1200

    server side

--- router.js
+++ router.js
@@ -1,47 +1,9 @@
 var request = require('superagent');
-var env = require('dotenv').config();
 
-console.log("You Are Cool.")
-
-
-request
-  .get('https://kristingillies.nationbuilder.com/api/v1/people/match?email=kristin@forpurpose.co.nz&access_token=209761d26ceb76a83161a58045cc90ad8c2fdc608e7714381a9c9e055fec249e')
-  .set('Accept', 'application/json')
-  .end(function(err, res){
-    if (err) {
-      console.log('Oh no! error', err );
-    } else {
-      //console.log('yay got ' + JSON.stringify(res.body))
-      console.log('Diving Deeper: ' + JSON.stringify(res.body.person.first_name),
-                                      JSON.stringify(res.body.person.last_name),
-                                      JSON.stringify(res.body.person.phone));
-    }
-  });
+var newPerson = require('./views/newPerson.hbs')
 
 
-//////*****Create New Person****//////
-var postNow = function() {
-
-request
-  .post('https://kristingillies.nationbuilder.com/api/v1/people?access_token=209761d26ceb76a83161a58045cc90ad8c2fdc608e7714381a9c9e055fec249e')
-  .set('Accept', 'application/json')
-  .send({
-  "person": {
-    "email": "test@test.com",
-    "last_name": "Test",
-    "first_name": "Lizzie",
-    "sex": "M",
-    "employer": "Testingtons",
-  }
+xhr.get('/newPerson', function(req, res){
+  console.log("hi!")
+  res.render('newPerson')
 })
-  .end(function(err, res){
-    if (err) {
-      console.log("I'm sorry, there's been an error: ", err)
-    } else {
-      console.log("success: ", res)
-    }
-  })
-
-}
-
-//postNow();
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express')
 var app = express()
 
+console.log("Weeee! Listenin to your crazy code rants on port 5000")
+
 app.get('/', function (req, res) {
   res.send('Nationbuilder bitchez')
 })
+
 
 app.listen(5000)
