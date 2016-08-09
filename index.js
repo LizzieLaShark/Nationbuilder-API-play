9738a264cd2729e2777e66bcf8de6fafc02cdb59
lizzielashark <lizzie.s.sullivan@gmail.com>
2016-08-09 11:55:08 -0700

    reorganising and building basic pages

--- index.js
+++ index.js
@@ -1,49 +1,102 @@
-var request = require('superagent');
-var env = require('dotenv').config();
-var $ = require('jquery');
+// var request = require('superagent');
+// var env = require('dotenv').config();
+// var $ = require('jquery');
+//
+// var confirmAdd = require('../views/confirmAdd.hbs')
+// var newPerson = require('./views/newPerson.hbs')
+//
+//
+// request
+//   .get('https://kristingillies.nationbuilder.com/api/v1/people/match?email=kristin@forpurpose.co.nz&access_token=209761d26ceb76a83161a58045cc90ad8c2fdc608e7714381a9c9e055fec249e')
+//   .set('Accept', 'application/json')
+//   .end(function(err, res){
+//     if (err) {
+//       console.log('Oh no! error', err );
+//     } else {
+//       //console.log('yay got ' + JSON.stringify(res.body))
+//       console.log('Diving Deeper: ' + JSON.stringify(res.body.person.first_name),
+//                                       JSON.stringify(res.body.person.last_name),
+//                                       JSON.stringify(res.body.person.phone));
+//     }
+//   });
+//
+// //////*****Create New Person (hardcode)****//////
+// var postNow = function() {
+//   request
+//     .post('https://kristingillies.nationbuilder.com/api/v1/people?access_token=209761d26ceb76a83161a58045cc90ad8c2fdc608e7714381a9c9e055fec249e')
+//     .set('Accept', 'application/json')
+//     .send({
+//     "person": {
+//       "email": "test@test.com",
+//       "last_name": "Test",
+//       "first_name": "Test2",
+//       "sex": "M",
+//       "employer": "Test2ingtons",
+//     } // need to create form and send info from form in JSON format
+//   })
+//     .end(function(err, res){
+//       if (err) {
+//         console.log("I'm sorry, there's been an error: ", err)
+//       } else {
+//         console.log("success: ", res)
+//       }
+//     })
+// }
+//
+// postNow();
+//
+// //////**** Create Three Basic Pages ****//////
+//
+// var newPage = function {
+//
+//   request
+//   .post(https://kristingillies.nationbuilder.com/api/v1/sites/kristingillies/pages/basic_pages )
+//   .set('Accept', 'application/json')
+//   .send ({
+//     "basic_page": {
+//     "name": "Why Join Unite?",
+//     "content": "  <H1>Why you should join Unite</h1>
+//         <p>
+//             <h2>Being part of Unite makes your working life easier</h2>
+//
+//             Negotiation of your wages and conditions<br>
+//             Learn about your rights as a worker<br>
+//             Enforcement of your legal and work rights<br>
+//             Representation with any work problems<br>
+//             Legal Advocacy for personal grievances<br>
+//             Regular visits by a paid organiser to your worksite<br>
+//             Elected delegates on your worksite to assist you<br>
+//             A range of free and discounted benefits<br>
+//             You decide your Employment Agreement<br>
+//             You vote on major Unite decisions<br>
+//             You can become a workplace representative<br>
+//             You vote or can stand for leadership positions<br>
+//             You can choose to join the Unite Active Members Network and really get involved in helping others<br>
+//           </p>
+//         </h1>",
+//     "status": "published"
+//   }})
+//   .end(function(err, res){
+//     if (err) {
+//       console.log("I'm sorry, there's been an error: ", err)
+//     } else {
+//       console.log("success: ", res)
+//     }
+//   })
+// }
+//
+// newPage()
 
-var confirmAdd = require('../views/confirmAdd.hbs')
-var newPerson = require('./views/newPerson.hbs')
 
 
-request
-  .get('https://kristingillies.nationbuilder.com/api/v1/people/match?email=kristin@forpurpose.co.nz&access_token=209761d26ceb76a83161a58045cc90ad8c2fdc608e7714381a9c9e055fec249e')
-  .set('Accept', 'application/json')
-  .end(function(err, res){
-    if (err) {
-      console.log('Oh no! error', err );
-    } else {
-      //console.log('yay got ' + JSON.stringify(res.body))
-      console.log('Diving Deeper: ' + JSON.stringify(res.body.person.first_name),
-                                      JSON.stringify(res.body.person.last_name),
-                                      JSON.stringify(res.body.person.phone));
-    }
-  });
 
-//////*****Create New Person (hardcode)****//////
-var postNow = function() {
-  request
-    .post('https://kristingillies.nationbuilder.com/api/v1/people?access_token=209761d26ceb76a83161a58045cc90ad8c2fdc608e7714381a9c9e055fec249e')
-    .set('Accept', 'application/json')
-    .send({
-    "person": {
-      "email": "test@test.com",
-      "last_name": "Test",
-      "first_name": "Lizzie",
-      "sex": "M",
-      "employer": "Testingtons",
-    } // need to create form and send info from form in JSON format
-  })
-    .end(function(err, res){
-      if (err) {
-        console.log("I'm sorry, there's been an error: ", err)
-      } else {
-        console.log("success: ", res)
-      }
-    })
-}
 
-//postNow();
+
+
+
+
+
+
 
 //////***** Make a Form that Creates People *****////////
 
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express')
 var app = express()
 var hbs = require('hbs')
+var request = require('superagent');
+var env = require('dotenv').config();
+var $ = require('jquery');
+
+// var confirmAdd = require('../views/confirmAdd.hbs')
+// var newPerson = require('./views/newPerson.hbs')
+
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
@@ -12,6 +19,7 @@ app.get('/', function (req, res) {
 })
 
 //*note to self this only works as get request*//
+
 app.get('/newperson', function (req, res) {
   console.log("U R Kool")
   res.render('newPerson')
@@ -20,6 +28,69 @@ app.get('/newperson', function (req, res) {
 app.get('/confirmAdd', function(req, res){
   res.render('confirmAdd')
 })
+
+/////**** Find a Person ****//////
+
+// request
+//   .get('https://kristingillies.nationbuilder.com/api/v1/people/match?email=kristin@forpurpose.co.nz&access_token=209761d26ceb76a83161a58045cc90ad8c2fdc608e7714381a9c9e055fec249e')
+//   .set('Accept', 'application/json')
+//   .end(function(err, res){
+//     if (err) {
+//       console.log('Oh no! error', err );
+//     } else {
+//       //console.log('yay got ' + JSON.stringify(res.body))
+//       console.log('Diving Deeper: ' + JSON.stringify(res.body.person.first_name),
+//                                       JSON.stringify(res.body.person.last_name),
+//                                       JSON.stringify(res.body.person.phone));
+//     }
+//   })
+
+//////*****Create New Person (hardcode)****//////
+
+// var postNow = function() {
+//   request
+//     .post('https://kristingillies.nationbuilder.com/api/v1/people?access_token=209761d26ceb76a83161a58045cc90ad8c2fdc608e7714381a9c9e055fec249e')
+//     .set('Accept', 'application/json')
+//     .send({
+//     "person": {
+//       "email": "test2@test.com",
+//       "last_name": "Test2",
+//       "first_name": "Test2",
+//       "sex": "M",
+//       "employer": "Test2ingtons",
+//     } // need to create form and send info from form in JSON format
+//   })
+//     .end(function(err, res){
+//       if (err) {
+//         console.log("I'm sorry, there's been an error creating a person: ", err)
+//       } else {
+//         console.log("success: ", res)
+//       }
+//     })
+// }
+
+//postNow();
+
+//////**** Create Three Basic Pages ****//////
+
+request
+  .post('https://kristingillies/api/v1/sites/1/pages/basic_pages')
+  .set('Accept', 'application/json')
+  .send ({
+      "basic_page": {
+        "name": "A page here",
+        "content": "<p>some content</p>",
+        "status": "published"
+      }
+    })
+  .end(function(err, res){
+    if (err) {
+      console.log("I'm sorry, there's been an error: ", err)
+    } else {
+      console.log("success: ", res)
+    }
+  })
+
 
 
 app.listen(5000)
